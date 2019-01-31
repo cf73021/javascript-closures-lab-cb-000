a4e6f5bb8e68b528fb92008086d1c8a9433a975b
Chris Franklin <cf73021@gmail.com>
2019-01-31 17:32:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = "I don't do much."
 function bumpCounter(){
   const counter = 0
-  function addBump(){
-
+  function addBump(counter){
+    return counter++
   }
   function getBumps(){
 
